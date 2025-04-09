bc7cd00ae1ddf2cb57ad07b7953d0b15083c4119
hcghoanghuynguyen <hoanghuy.nguyen@the-hash.com>
2025-04-09 15:42:21 +0700

    fix main create world boundary

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,6 @@ import {
   Vector3,
   Color,
   Fog,
-  Mesh,
-  BoxGeometry,
-  MeshBasicMaterial,
-  DoubleSide,
   PCFSoftShadowMap,
   Object3D,
   Group,
@@ -37,75 +33,12 @@ import { createTerrain } from "./core/terrain";
 import { setupLighting } from "./core/lighting";
 import { populateEnvironment } from "./core/environment";
 import { createExitPortal, createStartPortal } from "./objects/portals";
+import { createWorldBoundary } from "./objects/walls.ts";
 import {
   spawnParticleEffect,
   updateParticleEffects,
 } from "./systems/particles";
 import { AIController } from "./entities/ai.ts";
-
-function createWorldBoundary(
-  scene: Scene,
-  worldSize: number,
-  collidableObjects: Object3D[]
-): void {
-  const thickness = 20;
-  const height = 100;
-  const halfSize = worldSize / 2;
-  const boundaryMaterial = new MeshBasicMaterial({
-    transparent: true,
-    opacity: 0.0,
-    side: DoubleSide,
-    visible: false,
-  });
-  const createWall = (
-    px: number,
-    pz: number,
-    sx: number,
-    sz: number,
-    name: string
-  ) => {
-    const wallGeo = new BoxGeometry(sx, height, sz);
-    const wallMesh = new Mesh(wallGeo, boundaryMaterial);
-    wallMesh.position.set(px, height / 2, pz);
-    wallMesh.name = name;
-    wallMesh.userData.isCollidable = true;
-    wallMesh.geometry.computeBoundingBox();
-    wallMesh.updateMatrixWorld(true);
-    wallMesh.userData.boundingBox = wallMesh.geometry
-      .boundingBox!.clone()
-      .applyMatrix4(wallMesh.matrixWorld);
-    scene.add(wallMesh);
-    collidableObjects.push(wallMesh);
-  };
-  createWall(
-    halfSize + thickness / 2,
-    0,
-    thickness,
-    worldSize + thickness * 2,
-    "Boundary+X"
-  );
-  createWall(
-    -halfSize - thickness / 2,
-    0,
-    thickness,
-    worldSize + thickness * 2,
-    "Boundary-X"
-  );
-  createWall(
-    0,
-    halfSize + thickness / 2,
-    worldSize + thickness * 2,
-    thickness,
-    "Boundary+Z"
-  );
-  createWall(
-    0,
-    -halfSize - thickness / 2,
-    worldSize + thickness * 2,
-    thickness,
-    "Boundary-Z"
-  );
-}
 
 export class Game {
   scene: Scene | null = null;
